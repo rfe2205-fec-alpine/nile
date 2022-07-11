9ea184b6ffe33cb9a53ef671b9413504f989a359
Sam Bartlett <96403295+samkbe@users.noreply.github.com>
2022-07-11 19:45:39 -0400

    Added reviews file directory, and added reviews component to app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import Overview from './components/overview/overview.jsx';
+import Reviews from './components/reviews/reviews.jsx';
 
 function App() {
   return (
     <PageContainer>
       <AppContainer>
         <Overview />
+        <Reviews />
       </AppContainer>
     </PageContainer>
   );
 }
-
 
 const PageContainer = styled.div`
   display: grid;
