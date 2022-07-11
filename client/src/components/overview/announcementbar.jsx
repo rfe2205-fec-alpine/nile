0579a8ef66802101af1a4756274a775d9fbca35b
Anthony Cella <anthony.cella242@gmail.com>
2022-07-11 14:22:49 -0400

    Created announcementbar.jsx

---
+++ client/src/components/overview/announcementbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function AnnouncementBar() {
+  return <div>Announcements!</div>
+}
+
+export default AnnouncementBar;
--- client/src/components/overview/overview.jsx
+++ client/src/components/overview/overview.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import TitleBar from './titlebar.jsx';
+import AnnouncementBar from './announcementbar.jsx';
 
 function Overview() {
   return (
     <OverviewComponent>
       <TitleBar />
+      <AnnouncementBar />
     </OverviewComponent>
   );
 }
