9a3ec71fa20f19788f9e48e6b759b197943b6024
Sam Bartlett <96403295+samkbe@users.noreply.github.com>
2022-07-12 13:58:28 -0400

    Created stubs for components

--- client/src/components/reviews/reviews.jsx
+++ client/src/components/reviews/reviews.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import RatingBreakdown from './ratingBreakdown/ratingBreakdown.jsx';
+import ReviewsList from './reviewsList/reviewsList.jsx';
+import ReviewButtons from './reviewsList/reviewButtons.jsx';
 
 function Reviews() {
   return (
     <ReviewsWrapper>
-      <p>Here is a test for reviews</p>
+      <RatingBreakdownWrapper>
+        <RatingBreakdown />
+      </RatingBreakdownWrapper>
+      <ReviewsListWrapper>
+        <ReviewsList />
+        <ReviewButtons />
+      </ReviewsListWrapper>
     </ReviewsWrapper>
   );
 }
 
 const ReviewsWrapper = styled.div`
-  box-shadow: 2px 1px 35px;
-  margin: 40px;
-  background: white;
+  display: grid;
+  margin-top: 30px;
+  grid-template-columns: 1fr 2fr;
+  border: 1px solid red;
+`;
+
+const RatingBreakdownWrapper = styled.div`
+  grid-column-start: 1fr;
+  border: 1px solid red;
+`;
+
+const ReviewsListWrapper = styled.div`
+  grid-column-start: 2fr;
+  border: 1px solid red;
 `;
 
 export default Reviews;
---
+++ client/src/components/reviews/reviewsList/addreviews.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+function AddReviewsButton() {
+  return (
+    <button type="submit" value="submit">Add Reviews Button</button>
+  );
+}
+
+export default AddReviewsButton;
