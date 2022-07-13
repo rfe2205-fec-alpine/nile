78ecab388ebf866cc3e86952d47a5c638bfbb7b8
Sam Bartlett <96403295+samkbe@users.noreply.github.com>
2022-07-13 14:54:23 -0400

    Average Rating functionality complete

--- client/src/components/reviews/ratingBreakdown/ratingBreakdown.jsx
+++ client/src/components/reviews/ratingBreakdown/ratingBreakdown.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Axios from 'axios';
 import Summary from './summary.jsx';
@@ -9,21 +9,30 @@ import ProductContext from '../../../ProductContext.jsx';
 
 function RatingBreakdown() {
   const [productID] = useContext(ProductContext);
-  Axios({
-    method: 'get',
-    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/meta',
-    headers: {
-      Authorization: GITHUB_API_KEY,
-    },
-    params: {
-      product_id: productID,
-    },
-  }).then((res) => {
-    console.log('This is the response to server request: ', res.data);
-  });
+  const [reviewData, setReviewData] = useState(null);
+
+  useEffect(() => {
+    Axios({
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/meta',
+      headers: {
+        Authorization: GITHUB_API_KEY,
+      },
+      params: {
+        product_id: productID,
+      },
+    }).then((res) => {
+      setReviewData(res.data);
+    }).catch((err) => { console.log(err); });
+  }, [productID]);
+
+  if (!reviewData) {
+    return null;
+  }
+
   return (
     <RatingBreakdownWrapper>
-      <Summary />
+      <Summary ratings={reviewData.ratings} />
       <Breakdown />
       <ProductBreakdown />
     </RatingBreakdownWrapper>
--- client/src/components/reviews/ratingBreakdown/summary.jsx
+++ client/src/components/reviews/ratingBreakdown/summary.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Summary() {
+function Summary( {ratings} ) {
+  const scores = Object.values(ratings);
+  const scoresAdded = scores.map((item, index) => item *= (index + 1));
+  const scoresReduced = scoresAdded.reduce((prev, current) => prev + current, 0);
+  const scoresAmount = scores.reduce((prev, current) => parseInt(prev) + parseInt(current), 0);
+  const averageRating = (scoresReduced / scoresAmount).toFixed(2);
+
   return (
     <SummaryWrapper>
-      <h3>Summary</h3>
+      <h1>{averageRating}</h1>
     </SummaryWrapper>
   );
 }
 
 const SummaryWrapper = styled.div`
   border: 1px solid red;
-  padding 20px;
+  padding 5px;
   margin 5px;
 `;
 
