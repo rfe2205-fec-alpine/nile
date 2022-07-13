e62002d140e7c5ee26f6ec14d64baaf80bc88ebf
Sam Bartlett <96403295+samkbe@users.noreply.github.com>
2022-07-13 19:46:58 -0400

    End of 7/12

--- client/src/components/reviews/ratingBreakdown/breakdown.jsx
+++ client/src/components/reviews/ratingBreakdown/breakdown.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
+import { AiFillCaretDown } from 'react-icons/ai';
 
 function Breakdown() {
   return (
     <BreakdownWrapper>
-      <h4>Breakdown</h4>
+      <CharacterBar />
+      <CharacterBar />
     </BreakdownWrapper>
+  );
+}
+
+function CharacterBar() {
+  const Wrapper = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  };
+  const Quadrant = {
+    backgroundColor: 'Gainsboro',
+    height: '5px',
+    marginRight: '5px',
+    width: '80%',
+  };
+  const img = {
+    backgroundPosition: '50%',
+  };
+  return (
+    <div>
+      <h5>Name</h5>
+      <div style={Wrapper}>
+        <AiFillCaretDown />
+        <div style={Quadrant} />
+        <div style={Quadrant} />
+        <div style={Quadrant} />
+        <div style={Quadrant} />
+      </div>
+    </div>
   );
 }
 
 const BreakdownWrapper = styled.div`
 border: 1px solid red;
-padding 20px;
+padding 0px;
 margin 5px;
 `;
 
