6b272894446ba09ed7c05970ee0098736c6825cc
Ivan Moreno <Ivandmoreno1985@gmail.com>
2022-07-13 18:53:03 -0400

    doing some get request to see the data

--- client/src/components/related/relatedList/relatedList.jsx
+++ client/src/components/related/relatedList/relatedList.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
-import RelatedCard from './relatedCard.jsx'
+import { useEffect, useState } from 'react';
+import RelatedCard from './relatedCard.jsx';
+
+const axios = require('axios');
+const { GITHUB_API_KEY } = require('../../../../../config.js');
 
 function RelatedList() {
+  const [productList, setProductList] = useState();
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products',
+      headers: {
+        Authorization: GITHUB_API_KEY,
+      },
+    })
+      .then((products) => {
+        axios({
+          method: 'get',
+          url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${products.data[0].id}/related`,
+          headers: {
+            Authorization: GITHUB_API_KEY,
+          },
+        })
+          .then((related) => {
+            console.log(related.data);
+            Promise.all(
+              related.data.map(relatedList => {
+                return axios({
+                  method: 'get',
+                  url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${relatedList}`,
+                  headers: {
+                    Authorization: GITHUB_API_KEY,
+                  },
+                });
+              }),
+            )
+              .then((data) => {
+                console.log(data);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <RelatedCard />
