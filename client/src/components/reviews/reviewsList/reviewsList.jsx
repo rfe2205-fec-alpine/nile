96a8f340cef0cfcbc73972aa80725bd37b858572
Samkbe <samkbe@gmail.com>
2022-07-14 19:45:34 -0400

    End of day 7/14

--- client/src/components/reviews/reviewsList/reviewsList.jsx
+++ client/src/components/reviews/reviewsList/reviewsList.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import styled from 'styled-components';
+import Axios from 'axios';
 import SortReviews from './sortReviews.jsx';
 import ReviewTile from './reviewTile.jsx';
 import ProductContext from '../../../ProductContext.jsx';
+import { GITHUB_API_KEY } from '../../../../../config.js';
 
 function ReviewsList() {
-  const [productID] = React.useContext(ProductContext);
+  const [productID] = useContext(ProductContext);
+  const [reviews, setReviews] = useState(null);
+  const [query, changeQuery] = useState('newest');
+
+  useEffect(() => {
+    Axios({
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews',
+      headers: {
+        Authorization: GITHUB_API_KEY,
+      },
+      params: {
+        product_id: productID,
+        sort: query,
+        count: 15,
+      },
+    }).then((res) => {
+      setReviews(res.data);
+    }).catch((err) => { console.log(err); });
+  }, [productID, query]);
+
+  if (!reviews) {
+    return null;
+  }
+
   return (
     <ReviewsListWrapper>
-      <h4>{productID}</h4>
-      <SortReviews />
-      <ReviewTile />
+      <SortReviews change={changeQuery} reviewQuery={query} />
+      {reviews.results.map((review) => {
+        return <ReviewTile key={review.review_id} reviewData ={review} />
+      })}
     </ReviewsListWrapper>
   );
 }
