f6588c208cbadcbcfe9508f8432e50f14ba651f2
Ivan Moreno <Ivandmoreno1985@gmail.com>
2022-07-14 19:54:07 -0400

    July 14 Merge Party

--- client/src/components/related/relatedList/relatedCard.jsx
+++ client/src/components/related/relatedList/relatedCard.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { FaStar } from 'react-icons/fa';
 import StarRating from '../starRating/starRating.jsx';
+import GetStyles from './relatedImage.jsx';
+
+const axios = require('axios');
+const { GITHUB_API_KEY } = require('../../../../../config.js');
 
 function RelatedCard({product}) {
+  const [rating, useRating] = useState(null);
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews?product_id=${product.id}`,
+      headers: {
+        Authorization: GITHUB_API_KEY,
+      },
+    })
+    .then((styles) => {
+      useRating(styles.data.results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
+  if (!rating) {
+    return null;
+  }
   return (
     <div>
       <Card>
         <StarButton><FaStar /></StarButton>
-        <ul>image</ul>
+        <GetStyles product={product} />
         <ul>{product.category}</ul>
         <ul>{product.name}</ul>
         <ul>{product.default_price}</ul>
-        <StarRating />
+        <StarRating rating={rating} />
       </Card>
     </div>
   );
