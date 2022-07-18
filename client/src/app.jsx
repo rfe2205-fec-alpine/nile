746c41c5007e99a03c9195385d19e950c4d12689
Anthony Cella <anthony.cella242@gmail.com>
2022-07-18 14:00:52 -0400

    Got the announcement bar to dynamically render based on color scheme

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -9,7 +9,7 @@ import ThemeContext from './ThemeContext.jsx';
 import Themes from './themes.js';
 
 function App() {
-  const selectedTheme = Themes.modern.light;
+  const selectedTheme = Themes.modern.dark;
   const [productId, setProductId] = useState('37314');
   const [colorScheme, setColorScheme] = useState(selectedTheme);
 
--- client/src/components/overview/title-section/announcementbar.jsx
+++ client/src/components/overview/title-section/announcementbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function AnnouncementBar() {
-  return <Announcement>SITE-WIDE ANNOUNCEMENT MESSAGE! SALE/DISCOUNT <strong>OFFER</strong> - <u>NEW PRODUCT HIGHLIGHT</u></Announcement>
+function AnnouncementBar({ colorScheme }) {
+  return <Announcement colorScheme={colorScheme}>SITE-WIDE ANNOUNCEMENT MESSAGE! SALE/DISCOUNT <strong>OFFER</strong> - <u>NEW PRODUCT HIGHLIGHT</u></Announcement>
 }
 
 const Announcement = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  color: #5d6699;
+  color: ${(props) => props.colorScheme.foreground};
   height: 50px;
 `;
 // color: #C8C3C2;
