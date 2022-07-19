deb07e5a4fefc650e22e76a4a033f6473dbc8f82
Anthony Cella <anthony.cella242@gmail.com>
2022-07-19 17:52:46 -0400

    Got the click tracker working

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+
+import { GITHUB_API_KEY, CAMPUS_CODE } from '../../config.js';
 import Overview from './components/overview/overview.jsx';
 import Reviews from './components/reviews/reviews.jsx';
 import QAndA from "./components/q&a/q&a.jsx";
@@ -8,6 +11,38 @@ import ProductContext from './ProductContext.jsx';
 import ThemeContext from './ThemeContext.jsx';
 import Themes from './themes.js';
 
+function ComponentWithClickTracker(Component, name) {
+  return function () {
+    function trackClick(event) {
+      const tag = event.target.tagName;
+      const date = (new Date()).toString();
+
+      const interactionsUrl = `https://app-hrsei-api.herokuapp.com/api/fec2/${CAMPUS_CODE}/interactions`;
+
+      axios({
+        method: 'post',
+        url: interactionsUrl,
+        data: {
+          element: tag,
+          widget: name,
+          time: date,
+        },
+        headers: {
+          Authorization: GITHUB_API_KEY,
+        },
+      })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    return <Component onClick={trackClick} />;
+  };
+}
+
 function App() {
   const selectedTheme = Themes.nile.light;
   const [productId, setProductId] = useState('37314');
@@ -15,12 +50,14 @@ function App() {
 
   document.body.style.backgroundColor = selectedTheme.bodyColor;
 
+  const ClickerOverview = ComponentWithClickTracker(Overview, 'Overview');
+
   return (
     <ThemeContext.Provider value={[colorScheme, setColorScheme]}>
       <ProductContext.Provider value={[productId, setProductId]}>
         <PageContainer>
           <AppContainer>
-            <Overview />
+            <ClickerOverview />
             <RelatedItems />
             <Reviews />
             <QAndA />
