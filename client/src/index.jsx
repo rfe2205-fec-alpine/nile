de66f9d1b686f9149b2ece8df2063214c9792460
Anthony Cella <anthony.cella242@gmail.com>
2022-07-19 12:13:16 -0400

    Started the click tracker function but got stuck

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./app.jsx";
 
+function AppWithClickTracker(Component) {
+  return function() {
+    function trackClick(event) {
+      let element = event.target;
+      let id = event.id;
+
+      console.log('element is', element);
+      console.log('id is', id);
+    }
+    return <Component onClick={trackClick} />;
+  };
+}
+
 render(<App />, document.getElementById('app'));
 
 // import React from 'react';
