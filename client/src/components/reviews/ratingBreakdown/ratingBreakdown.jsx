8ff516f8d0cee221ff086302b0874f15155c88e9
Samkbe <samkbe@gmail.com>
2022-07-21 10:20:03 -0400

    Removed some more comments...

--- client/src/components/reviews/ratingBreakdown/ratingBreakdown.jsx
+++ client/src/components/reviews/ratingBreakdown/ratingBreakdown.jsx
@@ -1,20 +1,10 @@
 import React, { useContext, useState, useEffect } from 'react';
 import styled from 'styled-components';
-import Axios from 'axios';
 import Summary from './summary.jsx';
 import Breakdown from './breakdown.jsx';
 import ProductBreakdown from './product-breakdown.jsx';
-import { GITHUB_API_KEY } from '../../../../../config.js';
-import ProductContext from '../../../ProductContext.jsx';
-import ReviewAmountContext from '../reviewAmountContext.jsx';
-import ReviewQualitiesContext from '../reviewQualities.jsx';
 
-function RatingBreakdown({ reviewData, setReviewData }) {
-  // const [productID] = useContext(ProductContext);
-  // const [, changeReviewAmount] = useContext(ReviewAmountContext);
-  // const [reviewData, setReviewData] = useState(null);
-  const [, changeReviewQualities] = useContext(ReviewQualitiesContext);
-
+function RatingBreakdown({ reviewData }) {
   return (
     <RatingBreakdownWrapper>
       {reviewData ? (
