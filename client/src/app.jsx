9c9c3142aed768119afafba243cf332bbeabc761
Anthony Cella <anthony.cella242@gmail.com>
2022-07-21 16:23:53 -0400

    Got stuff modified

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
+// import axios from 'axios';
 
 import { GITHUB_API_KEY, CAMPUS_CODE } from '../../config.js';
 import Overview from './components/overview/overview.jsx';
@@ -11,37 +11,37 @@ import ProductContext from './ProductContext.jsx';
 import ThemeContext from './ThemeContext.jsx';
 import Themes from './themes.js';
 
-function ComponentWithClickTracker(Component, name) {
-  return function () {
-    function trackClick(event) {
-      const tag = event.target.tagName;
-      const date = (new Date()).toString();
+// function ComponentWithClickTracker(Component, name) {
+//   return function () {
+//     function trackClick(event) {
+//       const tag = event.target.tagName;
+//       const date = (new Date()).toString();
 
-      const interactionsUrl = `https://app-hrsei-api.herokuapp.com/api/fec2/${CAMPUS_CODE}/interactions`;
+//       const interactionsUrl = `https://app-hrsei-api.herokuapp.com/api/fec2/${CAMPUS_CODE}/interactions`;
 
-      axios({
-        method: 'post',
-        url: interactionsUrl,
-        data: {
-          element: tag,
-          widget: name,
-          time: date,
-        },
-        headers: {
-          Authorization: GITHUB_API_KEY,
-        },
-      })
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
+//       axios({
+//         method: 'post',
+//         url: interactionsUrl,
+//         data: {
+//           element: tag,
+//           widget: name,
+//           time: date,
+//         },
+//         headers: {
+//           Authorization: GITHUB_API_KEY,
+//         },
+//       })
+//         .then((response) => {
+//           console.log(response);
+//         })
+//         .catch((error) => {
+//           console.log(error);
+//         });
+//     }
 
-    return <Component onClick={trackClick} />;
-  };
-}
+//     return <Component onClick={trackClick} />;
+//   };
+// }
 
 function App() {
   const selectedTheme = Themes.nile.light;
@@ -50,20 +50,20 @@ function App() {
 
   document.body.style.backgroundColor = selectedTheme.bodyColor;
 
-  const ClickerOverview = ComponentWithClickTracker(Overview, 'Overview');
-  const ClickerRelatedItems = ComponentWithClickTracker(RelatedItems, 'Related Items');
-  const ClickerReviews = ComponentWithClickTracker(Reviews, 'Reviews');
-  const ClickerQAndA = ComponentWithClickTracker(QAndA, 'Q&A');
+  // const ClickerOverview = ComponentWithClickTracker(Overview, 'Overview');
+  // const ClickerRelatedItems = ComponentWithClickTracker(RelatedItems, 'Related Items');
+  // const ClickerReviews = ComponentWithClickTracker(Reviews, 'Reviews');
+  // const ClickerQAndA = ComponentWithClickTracker(QAndA, 'Q&A');
 
   return (
     <ThemeContext.Provider value={[colorScheme, setColorScheme]}>
       <ProductContext.Provider value={[productId, setProductId]}>
         <PageContainer>
           <AppContainer>
-            <ClickerOverview />
-            <ClickerRelatedItems />
-            <ClickerReviews />
-            <ClickerQAndA />
+            <Overview />
+            <RelatedItems />
+            <Reviews />
+            <QAndA />
           </AppContainer>
         </PageContainer>
       </ProductContext.Provider>
